d0f2001d1075294cbb1b7b6195c66c8f117d7ad4
Hen-su <henleysu@outlook.com>
2023-09-20 01:47:31 +1200

    Add project files.

---
+++ GroovyGoodsWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GroovyGoodsWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
