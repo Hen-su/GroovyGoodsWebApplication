efea8b7b42ab95d14de6fa0158a8cafb85321cab
Hen-su <henleysu@outlook.com>
2023-09-23 00:45:11 +1200

    added database connection

--- GroovyGoodsWebApplication/Program.cs
+++ GroovyGoodsWebApplication/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<GroovyGoodsContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnectoin")));
+    builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
